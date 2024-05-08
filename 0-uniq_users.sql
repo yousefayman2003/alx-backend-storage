939c0cdd52be9de09b0d9548de46cddaf84bc796
Yousef Ayman <yousefayman20003@gmail.com>
2024-05-08 12:25:58 -0700

    remove files

--- 0-uniq_users.sql
+++
@@ -1,5 +0,0 @@
--- First Task
--- Write a SQL script that creates a table with spec req
-CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255));
