9f27708d604fc5cc8e5a97b728e70085b6a7ff71
Yousef Ayman <yousefayman20003@gmail.com>
2024-05-08 12:39:38 -0700

    upload task 8

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-	DECLARE avgScore FLOAT;
+	DECLARE avg_score FLOAT;
 	SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
 	UPDATE users SET average_score = avg_score WHERE id = user_id;
 END
