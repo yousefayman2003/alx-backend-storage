cbf51fefd13502e8d145945d522fea40383448a8
Yousef Ayman <yousefayman20003@gmail.com>
2024-05-08 12:21:27 -0700

    uplaod tasks

---
+++ 1-country_users.sql
@@ -0,0 +1,6 @@
+-- second Task
+-- Write a SQL script that creates a table with spec req
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN') NOT NULL);
