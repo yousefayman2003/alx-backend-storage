b0374d75bb9f8ac3c8499546f6f90867676681a1
Yousef <yousefayman20003@gmail.com>
2024-05-13 12:19:47 -0700

    upload tasks

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,5 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
-    mongo_collection.update_many({name: name}, {"$set": {
-        "topics": topics}})
+    mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}}
+    )
